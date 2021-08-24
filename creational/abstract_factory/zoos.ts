0d6129f6311d9ba896e12b2375a4c76365f0bce3
darkgraycat <jburyanov@gmail.com>
2021-08-24 10:09:48 +0300

    update zoos

--- creational/abstract_factory/zoos.ts
+++ creational/abstract_factory/zoos.ts
@@ -4,23 +4,17 @@ import { AnimalFactory, CatFactory, DogFactory } from './factories';
 export abstract class Zoo {
   abstract animals: Animal[];
   abstract factory: AnimalFactory;
-  abstract addAnimal(): void;
+  addAnimal(): void {
+    this.animals.push(this.factory.createAnimal());
+  }
 }
 
 export class CatZoo extends Zoo {
   animals: Animal[] = [];
   factory: AnimalFactory = new CatFactory();
-
-  addAnimal(): void {
-    this.animals.push(this.factory.createAnimal());
-  }
 }
 
 export class DogZoo extends Zoo {
   animals: Animal[] = [];
   factory: AnimalFactory = new DogFactory();
-
-  addAnimal(): void {
-    this.animals.push(this.factory.createAnimal());
-  }
 }
